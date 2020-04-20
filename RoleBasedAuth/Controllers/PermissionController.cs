34ae5851134a3928686ea2a20a36436fc3efa4c0
stanlylife <stianhave@hotmail.com>
2020-04-20 12:49:44 +0200

    added functionallity to permissions
    added option to create and add users to roles

--- RoleBasedAuth/Controllers/PermissionController.cs
+++ RoleBasedAuth/Controllers/PermissionController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using RoleBasedAuth.Data;
+using RoleBasedAuth.Models;
 
 namespace RoleBasedAuth.Controllers {
 
@@ -19,7 +21,15 @@ namespace RoleBasedAuth.Controllers {
 		}
 
 		[HttpGet]
-		public async Task<IActionResult> permissionAsync() {
+		public IActionResult permission() {
+			return View();
+		}
+
+		public IActionResult ButtonClick() {
+			return RedirectToAction("permission");
+		}
+
+		public async Task<IActionResult> getClaimsAsync() {
 			var loggedInUser = await userManager.GetUserAsync(User);
 			int i = 1;
 
@@ -28,13 +38,83 @@ namespace RoleBasedAuth.Controllers {
 				i++;
 			}
 
+			return RedirectToAction("permission");
+		}
+
+		public IActionResult getAllRoles() {
+			Console.WriteLine();
 			var roles = roleManager.Roles.ToList();
-			i = 0;
+			int i = 0;
 			foreach (var role in roles) {
 				Console.WriteLine($"Role number {i}: {role}");
+				i++;
 			}
+			return RedirectToAction("permission");
+		}
 
-			return View();
+		public async Task<IActionResult> getUserRolesAsync() {
+			Console.WriteLine();
+			var roles = roleManager.Roles.ToList();
+			var loggedInUser = await userManager.FindByNameAsync(User.Identity.Name.ToString());
+			int i = 0;
+			foreach (var ApplicationRole in roles) {
+				//https://stackoverflow.com/questions/40151224/user-isinrole-returns-nothing-in-asp-net-core-repository-pattern-implemented
+				//IsInRole checks cookie and not against database!
+				var role = ApplicationRole.ToString();
+
+				if (/*User.IsInRole(role.ToString())*/
+					await userManager.IsInRoleAsync(loggedInUser, role)
+					) {
+					Console.WriteLine($"{User.Identity.Name} is in Role number {i}: {role}");
+					i++;
+				} else {
+					Console.WriteLine($"{User.Identity.Name} is NOT in Role number {i}: {role}");
+					i++;
+				}
+			}
+			return RedirectToAction("permission");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateRolesAsync(PermissionModel model) {
+			if (!await roleManager.RoleExistsAsync(model.role)) {
+				await CreateOneRoleAsync(model.role);
+			}
+			return RedirectToAction("permission");
+		}
+
+		public async Task<IActionResult> AddUserToRoleAsync(PermissionModel model) {
+			var userToAppendRole = await userManager.FindByNameAsync(model.user);
+			if (!User.IsInRole(model.role.ToString())) { /*CHECK IF User is in role*/
+				var roles = roleManager.Roles.ToList();
+				bool roleExists = false;
+				string roleToAppendUser = model.role;
+				foreach (var role in roles) { /*CHECK IF Role exists*/
+					if (role.ToString().ToLower() == model.role.ToLower()) {
+						roleExists = true;
+						roleToAppendUser = role.ToString();
+					}
+				}
+				if (!roleExists) { /*IF NOT EXISTS Create role*/
+					var result = await CreateOneRoleAsync(model.role);
+					roleToAppendUser = result.ToString();
+				}
+
+				/*Add User to role*/
+				Console.WriteLine($"Added user to role {roleToAppendUser}");
+				await userManager.AddToRoleAsync(userToAppendRole, roleToAppendUser);
+			} else {
+				/*If user is already in role, return error*/
+				Console.WriteLine("user is already in role");
+				ModelState.AddModelError("All", $"{userToAppendRole} is already in {model.role}");
+			}
+			return RedirectToAction("permission");
+		}
+
+		public async Task<IdentityResult> CreateOneRoleAsync(string roleName) {
+			var role = new IdentityRole();
+			role.Name = roleName;
+			return await roleManager.CreateAsync(role);
 		}
 	}
 }
---
+++ RoleBasedAuth/Models/PermissionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoleBasedAuth.Models {
+
+	public class PermissionModel {
+		public string role { get; set; }
+		public string user { get; set; }
+	}
+}
