5d099a69c4cb478fc4dc97de0120820da3a90eaf
stanlylife <stianhave@hotmail.com>
2020-04-21 22:11:37 +0200

    updated startup
    configured services, added identity, dbcontext, authorization, authentication and configured cookies

--- Claims-With-IdentityUser/Startup.cs
+++ Claims-With-IdentityUser/Startup.cs
@@ -1,34 +1,71 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Claims_With_IdentityUser.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Claims_With_IdentityUser {
+
 	public class Startup {
+
 		public Startup(IConfiguration configuration) {
 			Configuration = configuration;
 		}
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
+			//Add database
+			services.AddDbContext<ApplicationDbContext>(options => {
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+			});
+
+			//Add identity
+			services.AddIdentity<IdentityUser, IdentityRole>(options => {
+				options.Password.RequireDigit = false;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequiredUniqueChars = 1;
+			}).AddEntityFrameworkStores<ApplicationDbContext>()
+			.AddDefaultTokenProviders();
+
+			services.ConfigureApplicationCookie(options => {
+				options.Cookie.Name = "my websites cookie";
+				options.AccessDeniedPath = "/home/denied";
+				options.LoginPath = "/home/login";
+			});
+			services.AddAuthentication(options => {
+			}).AddCookie("My Authentication schema", options => {
+				options.Cookie.Name = "my authentication cookie";
+				options.AccessDeniedPath = "/home/authenticationdenied";
+			});
+
+			services.AddAuthorization(options => {
+				options.AddPolicy("myWebsiteGenderPolicy", MyPolicyBuilder => {
+					MyPolicyBuilder.RequireClaim(ClaimTypes.Gender);
+				});
+
+				options.AddPolicy("myWebsiteAgePolicy", MyPolicyBuilder => {
+					MyPolicyBuilder.RequireClaim("Age", "21");
+				});
+			});
+
 			services.AddControllersWithViews();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if (env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			} else {
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 			app.UseHttpsRedirection();
@@ -36,6 +73,7 @@ namespace Claims_With_IdentityUser {
 
 			app.UseRouting();
 
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints => {
